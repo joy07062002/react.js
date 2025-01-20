2b1bfe78ab889d6bf30b22a4571b4412a1d9010a
joy07062002 <joy07062002@gmail.com>
2025-01-20 09:46:01 +0000

    8thproject

---
+++ 08minicontext/src/components/Profile.jsx
@@ -0,0 +1,9 @@
+import React,{useContext} from 'react'
+import UserContext from  '../context/UserContext'
+function profile(){
+    const{user}=useContext(UserContext)
+    if(!user)return <div>please login</div>
+    return <div>Welcome {user.username}</div>
+}
+
+export default profile
